08550ed715f4c68e154d1119db3c4f574d56bb62
Faris <fhrvo1@etf.unsa.ba>
2020-03-25 18:10:02 +0100

    dodano: klasa treba da poštuje JavaBean specifikaciju u smislu postojanja setter/getter metoda i konstruktora;

--- src/ba/unsa/etf/rs/tut4/Artikal.java
+++ src/ba/unsa/etf/rs/tut4/Artikal.java
@@ -4,4 +4,32 @@ public class Artikal {
         private String sifra;
         private String naziv;
         private double cijena;
+
+    public Artikal() {
+
+    }
+
+    public String getSifra() {
+        return sifra;
+    }
+
+    public void setSifra(String sifra) {
+        this.sifra = sifra;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public double getCijena() {
+        return cijena;
+    }
+
+    public void setCijena(double cijena) {
+        this.cijena = cijena;
+    }
 }
